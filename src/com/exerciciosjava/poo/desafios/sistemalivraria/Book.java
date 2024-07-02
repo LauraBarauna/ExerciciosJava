f574f86a695c286bb71983a1e688990c7219d803
LauraBarauna <lauravbarauna@gmail.com>
2024-07-02 17:56:13 -0300

    Criando código inicial da classe Book

--- src/com/exerciciosjava/poo/desafios/sistemalivraria/Book.java
+++ src/com/exerciciosjava/poo/desafios/sistemalivraria/Book.java
@@ -1,4 +1,45 @@
 package com.exerciciosjava.poo.desafios.sistemalivraria;
 
 public class Book {
+    //atributos
+    private int id;
+    private String title;
+    private String author;
+    private int year;
+
+    //construtor
+    public Book(int id, String title, String author, int year) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    //getters
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    //to string
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                '}';
+    }
 }
