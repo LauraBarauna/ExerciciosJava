4c369a6c3b92b15b77867c0d4e5add3f2d30fe42
LauraBarauna <lauravbarauna@gmail.com>
2024-07-02 18:04:40 -0300

    Criando código inicial da classe Library

--- src/com/exerciciosjava/poo/desafios/sistemalivraria/Library.java
+++ src/com/exerciciosjava/poo/desafios/sistemalivraria/Library.java
@@ -1,4 +1,37 @@
 package com.exerciciosjava.poo.desafios.sistemalivraria;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class Library {
+    //atributo
+    private List<Book> bookList;
+
+    //construtor
+    public Library() {
+        this.bookList = new ArrayList<>();
+    }
+
+    //métodos
+    public void addBook(Book book){
+        bookList.add(book);
+    }
+
+    public List findBookByTitle(String title){
+        ArrayList bookTitle = new ArrayList<>(bookList);
+        if (!bookList.isEmpty()){
+            for (Book b: bookList){
+                if (b.getTitle().equalsIgnoreCase(title)){
+                    bookTitle.add(b);
+                }
+            }
+        }
+        return bookTitle;
+    }
+
+    public void listAllBooks(){
+        System.out.println(bookList);
+    }
+
 }
